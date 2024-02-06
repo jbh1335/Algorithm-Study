ff0118f1bafd4956803ad7d44959603ea44d5644
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-02-06 23:54:30 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3068 ms, Memory: 428076 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -8,9 +8,7 @@ public class Main {
 
         for(int tc = 1; tc <= T; tc++) {
             int k = Integer.parseInt(br.readLine());
-            PriorityQueue<Integer> pqueUp = new PriorityQueue<>();
-            PriorityQueue<Integer> pqueDown = new PriorityQueue<>(Collections.reverseOrder());
-            HashMap<Integer, Integer> map = new HashMap<>();
+            TreeMap<Integer, Integer> map = new TreeMap<>();
 
             for(int i = 0; i < k; i++) {
                 String str = br.readLine();
@@ -18,40 +16,22 @@ public class Main {
                 int num = Integer.parseInt(splitArr[1]);
 
                 if(splitArr[0].equals("I")) {
-                    pqueUp.offer(num);
-                    pqueDown.offer(num);
                     map.put(num, map.getOrDefault(num, 0)+1);
                 } else {
                     if(map.isEmpty()) continue;
 
-                    if(num == -1) {
-                        delete(pqueUp, map);
-                    } else {
-                        delete(pqueDown, map);
-                    }
+                    if(num == -1) delete(map.firstKey(), map);
+                    else delete(map.lastKey(), map);
                 }
             }
 
             if(map.isEmpty()) System.out.println("EMPTY");
-            else {
-                int max = delete(pqueDown, map);
-                int min = map.isEmpty() ? max : delete(pqueUp, map);
-                System.out.println(max + " " + min);
-            }
+            else System.out.println(map.lastKey() + " " + map.firstKey());
         }
     }
 
-    public static int delete(PriorityQueue<Integer> pque, HashMap<Integer, Integer> map) {
-        int num = 0;
-        while(true) {
-            num = pque.poll();
-
-            if(map.containsKey(num)) {
-                if(map.get(num) == 1) map.remove(num);
-                else map.replace(num, map.get(num)-1);
-                break;
-            }
-        }
-        return num;
+    public static void delete(int key, TreeMap<Integer, Integer> map) {
+        if(map.get(key) == 1) map.remove(key);
+        else map.put(key, map.get(key)-1);
     }
 }
