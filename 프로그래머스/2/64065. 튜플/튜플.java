758c362de19b09c9b28c904ae87d250b77465632
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-02-11 22:54:21 +0900

    [level 2] Title: 튜플, Time: 0.61 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -5,20 +5,15 @@ class Solution {
         String[] splitArr = s.split("},");
         int[] answer = new int[splitArr.length];
         
-        ArrayList<String> list = new ArrayList<>();
-        for(int i = 0; i < splitArr.length; i++) {
-            list.add(splitArr[i]);
-        }
+        Arrays.sort(splitArr, (o1, o2) -> o1.length() - o2.length());
+        HashSet<Integer> set = new HashSet<>();
         
-        Collections.sort(list, (o1, o2) -> o1.length() - o2.length());
-        boolean[] visited = new boolean[100001];
-        for(int i = 0; i < list.size(); i++) {
-            String[] numArr = list.get(i).split(",");
+        for(int i = 0; i < splitArr.length; i++) {
+            String[] numArr = splitArr[i].split(",");
             for(String sNum : numArr) {
                 int num = Integer.parseInt(sNum);
-                if(!visited[num]) {
+                if(set.add(num)) {
                     answer[i] = num;
-                    visited[num] = true;
                     break;
                 }
             }
