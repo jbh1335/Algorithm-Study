faca4fe945bb969e43face44ca71e8b24d1c2753
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-02-14 23:11:11 +0900

    [level 2] Title: 스킬트리, Time: 7.44 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for(int i = 0; i < skill_trees.length; i++) {
+            String str = skill_trees[i].replaceAll("[^" + skill + "]", "");
+            
+            if(str.equals(skill)) answer++;
+            else {
+                boolean flag = true;
+                for(int j = 0; j < str.length(); j++) {
+                    if(skill.charAt(j) != str.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(flag) answer++;
+            }
+        }
+        return answer;
+    }
+}
