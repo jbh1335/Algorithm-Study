b70cefa99b1570463a2189df12c84817bf5949d5
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-02-24 20:56:52 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,6 +1,6 @@
 class Solution {
-    static int answer;
     public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
         // arrayA의 최대공약수
         int gcdA = arrayA[0];
         for(int i = 1; i < arrayA.length; i++) {
@@ -13,11 +13,11 @@ class Solution {
             gcdB = gcd(gcdB, arrayB[i]);
         }
         
-        // gcdA로 조건 탐색
-        divisor(gcdA, arrayB);
+        // gcdA가 arrayB에 나누어 떨어지는지 확인
+        if(checkOk(gcdA, arrayB)) answer = Math.max(answer, gcdA);
         
-        // gcdB로 조건 탐색
-        divisor(gcdB, arrayA);
+        // gcdB가 arrayA에 나누어 떨어지는지 확인
+        if(checkOk(gcdB, arrayA)) answer = Math.max(answer, gcdB);
         return answer;
     }
     
@@ -26,28 +26,11 @@ class Solution {
         return gcd(b, a%b);
     }
     
-    // 약수 중에 조건에 맞는 수 찾기
-    public static void divisor(int num, int[] arr) {
-        // 최대공약수가 조건에 맞으면 그 약수들은 확인하지 않아도 되므로 리턴
-        if(!checkDivided(num, arr)) {
-            answer = Math.max(answer, num);
-            return;
-        }
-        
-        int sqrt = (int) Math.sqrt(num);       
-        for(int i = 2; i <= sqrt; i++) {
-            if(num % i == 0) {
-                if(!checkDivided(i, arr)) answer = Math.max(answer, i);
-                if(!checkDivided(num/i, arr)) answer = Math.max(answer, num/i);
-            }
-        }
-    }
-    
     // 해당 배열에서 나누어 떨어지는 수가 있는지 확인
-    public static boolean checkDivided(int num, int[] arr) {
+    public static boolean checkOk(int num, int[] arr) {
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i] % num == 0) return true;
+            if(arr[i] % num == 0) return false;
         }
-        return false;
+        return true;
     }
 }
