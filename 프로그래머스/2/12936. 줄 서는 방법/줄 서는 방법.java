03fe2d851b47636cbf0b04c9339f2fe4fa51c136
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-02-25 23:55:34 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -13,19 +13,21 @@ class Solution {
         // 인덱스는 0부터 시작하므로 -1
         k--;
         
+        // 0번째 자리부터 시작해서 찾기
         while(n > 0) {
             // 전체개수 / 숫자개수(배열)
             // 각 숫자가 앞자리로 올 때 만들 수 있는 경우의 수: fact개
             fact /= n;
             
-            // 몇번째 요소인지
+            // 몇번째 요소인지 찾기
             int idx = (int) (k / fact);
             answer[cnt++] = list.get(idx);
+            // 숫자를 사용했으니 제거
             list.remove(idx);
             
             // 하나의 숫자를 뽑았으니 -1
             n--;
-            // 나머지가 그 다음 순서
+            // 나머지가 그 다음으로 찾아야 하는 순서
             k %= fact;
         }
         return answer;
