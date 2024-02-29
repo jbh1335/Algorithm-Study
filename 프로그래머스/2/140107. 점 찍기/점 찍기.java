091dfb927f8752cf36d9c746587d357a39f956fb
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-03-01 00:21:06 +0900

    [level 2] Title: 점 찍기, Time: 0.08 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,17 +1,10 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        long dist = (long) d * d;
         for(int i = 0; i <= d; i+=k) {
-            answer += findNum(i, k, dist);
+            int sqrt = (int) Math.sqrt((long) d * d - (long) i * i);
+            answer += sqrt/k + 1;
         }
         return answer;
-    }
-    
-    public static int findNum(int n, int k, long dist) {
-        long num = (long) n * n;
-        long minus = dist - num;
-        int sqrt = (int) Math.sqrt(minus);
-        return sqrt/k + 1;
     }
 }
