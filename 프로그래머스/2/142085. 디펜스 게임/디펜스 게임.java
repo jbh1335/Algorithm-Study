2b82f7edbe6aa8ecac4faab8054c74b8767b6442
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-03-01 15:15:22 +0900

    [level 2] Title: 디펜스 게임, Time: 0.74 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = -1;
+        // 무적권이 적보다 크거나 같으면 다 사용 가능
+        if(k >= enemy.length) return enemy.length;
+        // 적을 내림차순 정렬로 저장
+        PriorityQueue<Integer> pque = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0; i < enemy.length; i++) {
+            // 적 저장
+            pque.offer(enemy[i]);
+            // 병사수 감소
+            n -= enemy[i];
+            
+            // 만약 병사수가 0보다 작으면
+            if(n < 0) {
+                // 가장 큰 적(내림차순 정렬했음)을 제거하고 무적권을 사용해서 병사수 다시 증가 
+                if(k > 0) {
+                    n += pque.poll();
+                    k--;
+                } else {
+                    answer = i;
+                    break;
+                }
+            }
+        }
+        
+        if(answer == -1) {
+            answer = enemy.length;
+        }
+        return answer;
+    }
+}
