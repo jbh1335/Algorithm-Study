e3bae5425ff099ac46b18893a76b19389819a84f
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-03-07 12:46:41 +0900

    [level 3] Title: 불량 사용자, Time: 2.10 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,66 +1,60 @@
 import java.util.*;
 class Solution {
-    static int answer;
-    static boolean isFound;
     static String[] select;
+    static boolean[] visited;
+    static HashSet<String> answerSet;
     public int solution(String[] user_id, String[] banned_id) {
         select = new String[banned_id.length];
+        visited = new boolean[user_id.length];
+        answerSet = new HashSet<>();
         
-        com(0, 0, user_id, banned_id);
-        return answer;
+        per(0, user_id, banned_id);
+        return answerSet.size();
     }
     
-    public static void com(int cnt, int start, String[] user_id, String[] banned_id) {
+    // 순열로 뽑기
+    public static void per(int cnt, String[] user_id, String[] banned_id) {
         if(cnt == banned_id.length) {
-            isFound = false;
-            per(0, new boolean[cnt], new String[cnt], banned_id);
-            return;
-        }
-        
-        for(int i = start; i < user_id.length; i++) {
-            select[cnt] = user_id[i];
-            com(cnt+1, i+1, user_id, banned_id);
-        }
-    }
-    
-    public static void per(int cnt, boolean[] visited, String[] perSel, String[] banned_id) {
-        if(isFound) return;
-        if(cnt == select.length) {
-            boolean isOk = true;
-            loop:
-            for(int i = 0; i < cnt; i++) {
-                String id = perSel[i];
-                String target = banned_id[i];
+            // 조건이 맞으면
+            if(checkOk(user_id, banned_id)) {
+                StringBuilder sb = new StringBuilder();
+                String[] newSelect = select.clone();
+                // 오름차순 정렬하여 문자로 만들고 저장 -> 중복 제거를 위해
+                Arrays.sort(newSelect);
                 
-                if(id.length() != target.length()) {
-                    isOk = false;
-                    break;
+                for(String id : newSelect) {
+                    sb.append(id);
                 }
-                
-                for(int j = 0; j < id.length(); j++) {
-                    if(target.charAt(j) == '*') continue;
-                    if(id.charAt(j) != target.charAt(j)) {
-                        isOk = false;
-                        break loop;
-                    }
-                }
+                answerSet.add(sb.toString());
             }
             
-            if(isOk) {
-                isFound = true;
-                answer++;
-            }
             return;
         }
         
-        for(int i = 0; i < select.length; i++) {
-            if(isFound) return;
+        for(int i = 0; i < user_id.length; i++) {
             if(visited[i]) continue;
             
-            perSel[cnt] = select[i];
+            select[cnt] = user_id[i];
             visited[i] = true;
-            per(cnt+1, visited, perSel, banned_id);
+            per(cnt+1, user_id, banned_id);
             visited[i] = false;
         }
+    }
+    
+    // 조건에 맞는지 검사
+    public static boolean checkOk(String[] user_id, String[] banned_id) {
+        for(int i = 0; i < select.length; i++) {
+            String id = select[i];
+            String target = banned_id[i];
+
+            if(id.length() != target.length()) return false;
+
+            for(int j = 0; j < id.length(); j++) {
+                if(target.charAt(j) == '*') continue;
+                if(id.charAt(j) != target.charAt(j)) return false;
+            }
+        }
+
+        return true;
     }
 }
