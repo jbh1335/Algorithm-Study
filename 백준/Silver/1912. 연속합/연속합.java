52191c64d30cdfa1df4a37bb0fb5206cc5d8f46c
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-04-18 10:16:59 +0900

    [Silver II] Title: 연속합, Time: 192 ms, Memory: 21968 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,19 +4,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
 
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[n];
+        int[] dp = new int[N];
         dp[0] = arr[0];
         int max = dp[0];
-        for(int i = 1; i < n; i++) {
-            dp[i] = Math.max(arr[i], arr[i]+dp[i-1]);
+        for(int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
             max = Math.max(max, dp[i]);
         }
         System.out.println(max);
