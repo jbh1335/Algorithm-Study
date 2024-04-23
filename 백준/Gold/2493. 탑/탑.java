68328e5b1e9c8ae0d14f87139f606e4cd0aae4c9
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-04-23 10:13:34 +0900

    [Gold V] Title: 탑, Time: 676 ms, Memory: 130720 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -2,49 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Point {
-        int num, idx;
-        public Point(int num, int idx) {
-            this.num = num;
-            this.idx = idx;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         int[] arr = new int[N+1];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<Point> stack = new Stack<>();
-        int[] answer = new int[N+1];
+        // 앞 탑들의 인덱스를 저장
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= N; i++) {
-            if(stack.isEmpty()) {
-                stack.push(new Point(arr[i], i));
-                continue;
-            }
-
-            if(stack.peek().num > arr[i]) {
-                answer[i] = stack.peek().idx;
-            } else {
-                while(!stack.isEmpty()) {
-                    Point p = stack.peek();
-                    if(p.num > arr[i]) {
-                        answer[i] = p.idx;
-                        break;
-                    } else {
-                        stack.pop();
-                    }
+            boolean flag = false;
+            // 더 이상 검사할 탑이 없을 때까지 반복
+            while(!stack.isEmpty()) {
+                int idx = stack.peek();
+                if(arr[idx] > arr[i]) { // 자신보다 높은 탑이 존재하면 그만두기
+                    sb.append(idx + " ");
+                    flag = true;
+                    break;
+                } else { // 자신보다 낮은 탑은 필요 없으므로 제거
+                    stack.pop();
                 }
             }
-            stack.push(new Point(arr[i], i));
+            // 레이저 수신할 탑 없으면 0 저장
+            if(!flag) sb.append(0 + " ");
+            // 자기 자신 넣기
+            stack.push(i);
         }
-
-        for(int i = 1; i <= N; i++) {
-            System.out.print(answer[i] + " ");
-        }
+        System.out.println(sb);
     }
 }
