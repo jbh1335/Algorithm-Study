cb68f0ae135c10a791960f27d3d83cbadaff2532
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-07-15 20:55:45 +0900

    [level 0] Title: 글자 지우기, Time: 2.74 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,20 +1,14 @@
-import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        ArrayList<Character> list = new ArrayList<>();
-        Arrays.sort(indices);
+        String[] sArr = my_string.split("");
         
-        for(int i = 0; i < my_string.length(); i++) {
-            list.add(my_string.charAt(i));
+        for(int idx : indices) {
+            sArr[idx] = "";
         }
         
-        for(int i = indices.length-1; i >= 0; i--) {
-            list.remove(indices[i]);
-        }
-        
-        for(char ch : list) {
-            answer += ch;
+        for(String s : sArr) {
+            answer += s;
         }
         
         return answer;
