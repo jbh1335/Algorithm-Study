acb24a36bf62f6d47bfc4a830e04558c9c7e53b7
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-09-19 01:12:38 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12300 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,56 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M;
-	static int[][] graph;
-	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		graph = new int[N][M];
-		visited = new boolean[N][M];
-		
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < M; j++) {
-				graph[i][j] = Integer.valueOf(str.charAt(j)) - '0';
-			}
-		}
-		
-		bfs(0, 0);
-		
-		System.out.println(graph[N-1][M-1]);
-	}
-	
-	public static void bfs(int x, int y) {
-		Queue<int[]> que = new LinkedList<>();
-		visited[x][y] = true;
-		que.add(new int[] {x, y});
-		
-		while(!que.isEmpty()) {
-			int[] num = que.poll();
-			
-			for(int i = 0; i < 4; i++) {
-				int nx = num[0] + dx[i];
-				int ny = num[1] + dy[i];
-				
-				if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
-					if(!visited[nx][ny] && graph[nx][ny] == 1) {
-						visited[nx][ny] = true;
-						que.add(new int[] {nx, ny});
-						graph[nx][ny] = graph[num[0]][num[1]] + 1;
-					}
-				}
-			}
-		}
-		
-	}
+    static int N, M;
+    static int[][] map;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static class Point {
+        int x, y, dist;
+        public Point(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    public static int bfs() {
+        Queue<Point> que = new LinkedList<>();
+        que.offer(new Point(0, 0, 1));
+        map[0][0] = -1;
+
+        while(!que.isEmpty()) {
+            Point p = que.poll();
+
+            for(int d = 0; d < 4; d++) {
+                int nx = p.x + dx[d];
+                int ny = p.y + dy[d];
+
+                if(nx >= 0 && ny >= 0 && nx < N && ny < M && map[nx][ny] == 1) {
+                    if(nx == N-1 && ny == M-1) return p.dist +1;
+                    que.offer(new Point(nx, ny, p.dist +1));
+                    map[nx][ny] = -1;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
