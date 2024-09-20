23467541f512d5d258e20d74c2d4c3daf5099584
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-09-20 21:39:06 +0900

    [level 3] Title: 입국심사, Time: 124.05 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,25 +2,26 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-        
         Arrays.sort(times);
-        long start = 0, end = (long) times[times.length-1] * n;
+        
+        long start = 0, end = (long) n * times[times.length-1], mid = 0;
         
         while(start <= end) {
-            long mid = (start + end) / 2;
-            long num = 0;
-            // mid분동안 심사하면 총 몇명할 수 있는지 검사
-            for(int i = 0; i < times.length; i++) {
-                num += mid / times[i];
+            mid = (start + end) / 2;
+            
+            long count = 0;
+            for(int i : times) {
+                count += mid / i;
             }
-            // 가능
-            if(n <= num) {
-                answer = mid;
+            
+            if(count >= n) {
                 end = mid - 1;
+                answer = mid;
             } else {
                 start = mid + 1;
             }
         }
+        
         return answer;
     }
 }
