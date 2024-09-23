0fb40b5987c3044781891aa63183d40492a2dbf3
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-09-23 22:28:30 +0900

    [level 2] Title: 리코쳇 로봇, Time: 5.78 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,27 +1,28 @@
 import java.util.*;
 class Solution {
+    static int N, M;
     static char[][] map;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static class Point {
-        int x, y, dist;
-        public Point(int x, int y, int dist) {
+        int x, y, cnt;
+        public Point(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
+            this.cnt = cnt;
         }
     }
     public int solution(String[] board) {
-        int answer = 0;
-        map = new char[board.length][board[0].length()];
-        visited = new boolean[board.length][board[0].length()];
-        
+        N = board.length;
+        M = board[0].length();
+        map = new char[N][M];
+        visited = new boolean[N][M];
         int startX = 0, startY = 0;
-        for(int i = 0; i < board.length; i++) {
-            String str = board[i];
-            for(int j = 0; j < str.length(); j++) {
-                map[i][j] = str.charAt(j);
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                map[i][j] = board[i].charAt(j);
                 if(map[i][j] == 'R') {
                     startX = i;
                     startY = j;
@@ -29,44 +30,52 @@ class Solution {
             }
         }
         
-        answer = bfs(startX, startY);
+        int answer = bfs(startX, startY);
         return answer;
     }
     
-    public static int bfs(int startX, int startY) {
+    public static int bfs(int x, int y) {
         Queue<Point> que = new LinkedList<>();
-        que.offer(new Point(startX, startY, 0));
-        visited[startX][startY] = true;
+        que.offer(new Point(x, y, 0));
+        visited[x][y] = true;
         
         while(!que.isEmpty()) {
             Point p = que.poll();
             
             for(int d = 0; d < 4; d++) {
-                int x = p.x;
-                int y = p.y;
+                int nx = p.x;
+                int ny = p.y;
+                boolean flag = false;
+                
                 while(true) {
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
+                    nx += dx[d];
+                    ny += dy[d];
                     
-                    if(nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length) {
+                    if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
                         if(map[nx][ny] == 'D') {
-                            break;
-                        } else {
-                            x = nx;
-                            y = ny;
+                            flag = true;
                         }
                     } else {
+                        flag = true;
+                    }
+                    
+                    if(flag) {
+                        nx -= dx[d];
+                        ny -= dy[d];
+                        
+                        if(map[nx][ny] == 'G') return p.cnt + 1;
+                        
+                        if(!visited[nx][ny]) {
+                            que.offer(new Point(nx, ny, p.cnt+1));
+                            visited[nx][ny] = true;
+                        }
+                        
                         break;
                     }
                 }
-                
-                if(map[x][y] == 'G') return p.dist+1;
-                if(!visited[x][y]) {
-                    que.offer(new Point(x, y, p.dist+1));
-                    visited[x][y] = true;
-                }
             }
         }
+        
         return -1;
     }
 }
