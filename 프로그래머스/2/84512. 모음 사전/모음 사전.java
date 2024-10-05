cd618b000b8c7730562d73b9f450e06b2e16cdfc
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-05 18:22:28 +0900

    [level 2] Title: 모음 사전, Time: 3.85 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,26 @@
 class Solution {
     static int answer;
     static boolean isFound;
-    static char[] alpha = {'A', 'E', 'I', 'O', 'U'};
+    static String[] alpha = {"A", "E", "I", "O", "U"};
     public int solution(String word) {
-        find("", 0, word);
+        per(0, "", word);
         return answer;
     }
     
-    public static void find(String str, int cnt, String word) {
-        if(isFound || cnt == 5) return;
+    public static void per(int cnt, String str, String word) {
+        if(str.equals(word)) {
+            isFound = true;
+            return;
+        }
+        
+        if(!isFound && cnt == 5) {
+            return;
+        }
         
         for(int i = 0; i < 5; i++) {
             if(isFound) return;
-            String newStr = str + alpha[i];
             answer++;
-            
-            if(newStr.equals(word)) {
-                isFound = true;
-                return;
-            }
-            find(newStr, cnt+1, word);
+            per(cnt+1, str+alpha[i], word);
         }
     }
 }
