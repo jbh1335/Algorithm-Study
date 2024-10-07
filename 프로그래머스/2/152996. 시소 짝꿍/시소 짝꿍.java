c87324b812fa39fd4fc89f9ad859b1a3bb5ac515
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-07 17:58:28 +0900

    [level 2] Title: 시소 짝꿍, Time: 6854.01 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -3,26 +3,20 @@ class Solution {
     public long solution(int[] weights) {
         long answer = 0;
         Arrays.sort(weights);
-        int count = 0;
         
         for(int i = 0; i < weights.length-1; i++) {
-            int a = weights[i];
-            // 똑같은 숫자가 나오면 그전의 경우와 같으므로 -1을 한 개수만큼 추가하고 끝내기
-            if(i != 0 && a == weights[i-1]) {
-                // answer += (count - 1) 로 하면 안됨
-                // 같은 숫자가 연속으로 3번나오면 3번째는 -1이 한번 더 적용되지 않음
-                answer += --count;
-                continue;
-            }
-            
-            count = 0;
+            int target = weights[i];
             for(int j = i+1; j < weights.length; j++) {
-                int b = weights[j];
-                if(a == b || 2*a == b || 3*a == 2*b || 4*a == 3*b) count++;
+                // 1:1 2:1 3:2 4:3
+                if(target == weights[j] || target*2 == weights[j] || 
+                   target*3 == weights[j]*2 || target*4 == weights[j]*3) {
+                    answer++;
+                }
+                
+                if(target*2 < weights[j]) break;
             }
-            
-            answer += count;
         }
+        
         return answer;
     }
 }
