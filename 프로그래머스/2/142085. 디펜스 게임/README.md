3644d9098669da059781af883ca4d97e57717be9
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-08 20:55:52 +0900

    [level 2] Title: 디펜스 게임, Time: 139.75 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.79 ms
+메모리: 139 MB, 시간: 139.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 01일 15:17:39
+2024년 10월 08일 20:55:48
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,20 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        // 무적권이 적보다 크거나 같으면 다 사용 가능
         if(k >= enemy.length) return enemy.length;
-        // 적을 내림차순 정렬로 저장
-        PriorityQueue<Integer> pque = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pque = new PriorityQueue<>((o1, o2) -> o2 - o1);
         
         for(int i = 0; i < enemy.length; i++) {
-            // 적 저장
             pque.offer(enemy[i]);
-            // 병사수 감소
             n -= enemy[i];
             
-            // 만약 병사수가 0보다 작으면
             if(n < 0) {
-                // 가장 큰 적(내림차순 정렬했음)을 제거하고 무적권을 사용해서 병사수 다시 증가 
                 if(k > 0) {
                     n += pque.poll();
                     k--;
@@ -23,8 +17,10 @@ class Solution {
                     break;
                 }
             }
+            
             answer++;
         }
+        
         return answer;
     }
 }
