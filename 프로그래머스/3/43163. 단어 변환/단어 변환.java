496842021e21e5b28711e373b91d0e7d334b1716
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-12 16:23:41 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,17 +2,16 @@ import java.util.*;
 class Solution {
     static class Point {
         String word;
-        int dist;
+        int cnt;
         boolean[] visited;
-        public Point(String word, int dist, boolean[] visited) {
+        public Point(String word, int cnt, boolean[] visited) {
             this.word = word;
-            this.dist = dist;
+            this.cnt = cnt;
             this.visited = visited;
         }
     }
     public int solution(String begin, String target, String[] words) {
-        int answer = bfs(begin, target, words);
-        return answer;
+        return bfs(begin, target, words);
     }
     
     public static int bfs(String begin, String target, String[] words) {
@@ -23,23 +22,26 @@ class Solution {
             Point p = que.poll();
             
             for(int i = 0; i < words.length; i++) {
-                if(!p.visited[i] && checkOneDiff(p.word, words[i])) {
-                    if(words[i].equals(target)) return p.dist+1;
+                if(!p.visited[i] && isOneDiff(p.word, words[i])) {
+                    if(words[i].equals(target)) return p.cnt+1;
                     
                     p.visited[i] = true;
-                    que.offer(new Point(words[i], p.dist+1, p.visited));
+                    que.offer(new Point(words[i], p.cnt+1, p.visited));
                 }
             }
         }
+        
         return 0;
     }
     
-    public static boolean checkOneDiff(String str1, String str2) {
+    public static boolean isOneDiff(String str1, String str2) {
         int count = 0;
+        
         for(int i = 0; i < str1.length(); i++) {
             if(str1.charAt(i) != str2.charAt(i)) count++;
             if(count > 1) return false;
         }
+        
         return true;
     }
 }
