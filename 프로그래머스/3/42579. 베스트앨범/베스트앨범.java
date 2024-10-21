be03eaa02cd4485d37484ccf5d2e05f4b087f4d4
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-21 16:15:54 +0900

    [level 3] Title: 베스트앨범, Time: 3.89 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,47 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        // 장르별로 가장 많이 재생된 노래 2개씩 모아 베스트 앨범 출시
+        // 장르별 총 재생 횟수 저장하는 playMap
+        HashMap<String, Integer> playMap = new HashMap<>();
+        // 장르별 재생 횟수, 인덱스 저장하는 genreMap
+        HashMap<String, ArrayList<Integer>> genreMap = new HashMap<>();
         
-        // 장르, 재생 횟수
-        HashMap<String, Integer> playedMap = new HashMap<>();
-        // String: 장르, int[]: 재생 횟수, 고유번호
-        HashMap<String, ArrayList<int[]>> musicMap = new HashMap<>();
         for(int i = 0; i < genres.length; i++) {
-            // 장르마다 재생 횟수 누적
-            playedMap.put(genres[i], playedMap.getOrDefault(genres[i], 0) + plays[i]);
-            
-            // 기존 list 있으면 가져오기
-            ArrayList<int[]> list = new ArrayList<>();
-            if(musicMap.containsKey(genres[i])) list = musicMap.get(genres[i]);
-            
-            // 장르에 각 노래의 재생 횟수, 고유번호 저장
-            list.add(new int[] {plays[i], i});
-            musicMap.put(genres[i], list);
+            playMap.put(genres[i], playMap.getOrDefault(genres[i], 0) + plays[i]);
+            ArrayList<Integer> list = genreMap.containsKey(genres[i]) ? genreMap.get(genres[i]) : new ArrayList<>();
+            list.add(i);
+            genreMap.put(genres[i], list);
         }
         
-        // 1. 속한 노래가 많이 재생된 장르를 먼저 수록
-        ArrayList<String> playedList = new ArrayList<>(playedMap.keySet());
-        Collections.sort(playedList, (o1, o2) -> playedMap.get(o2) - playedMap.get(o1));
+        ArrayList<String> playList = new ArrayList<>(playMap.keySet());
+        Collections.sort(playList, (o1, o2) -> playMap.get(o2) - playMap.get(o1));
         
-        ArrayList<Integer> resultList = new ArrayList<>();
-        for(String genre : playedList) {
-            ArrayList<int[]> musicList = musicMap.get(genre);  
-            Collections.sort(musicList, (arr1, arr2) -> {
-                // 2. 장르 내에서 많이 재생된 노래를 먼저 수록
-                // 3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록
-                if(arr1[0] == arr2[0]) return arr1[1] - arr2[1];
-                return arr2[0] - arr1[0];
-            });
+        ArrayList<Integer> answerList = new ArrayList<>();
+        for(String genre : playList) {
+            ArrayList<Integer> list = genreMap.get(genre);
+            Collections.sort(list, (o1, o2) -> plays[o2] - plays[o1]);
             
-            resultList.add(musicList.get(0)[1]);
-            if(musicList.size() >= 2) resultList.add(musicList.get(1)[1]);
+            for(int i = 0; i < list.size(); i++) {
+                answerList.add(list.get(i));
+                if(i == 1) break;
+            }
         }
         
-        int[] answer = new int[resultList.size()];
+        int[] answer = new int[answerList.size()];
         for(int i = 0; i < answer.length; i++) {
-            answer[i] = resultList.get(i);
+            answer[i] = answerList.get(i);
         }
+        
         return answer;
     }
 }
