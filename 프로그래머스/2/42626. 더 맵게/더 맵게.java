89c6b68b2e918bf567c0e182993b94445428244f
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-22 19:13:11 +0900

    [level 2] Title: 더 맵게, Time: 1563.87 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,26 +3,20 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pque = new PriorityQueue<>();
-        for(int i : scoville) {
-            pque.offer(i);
+        
+        for(int num : scoville) {
+            pque.offer(num);
         }
         
-        boolean flag = false;
-        while(true) {
-            if(pque.peek() >= K) {
-                flag = true;
-                break;
-            }
+        while(pque.size() >= 2) {
+            if(pque.peek() >= K) break;
             
-            if(pque.size() == 1) break;
-            
-            // 새로운 스코빌 지수: 가장 작은 수 + (2번째로 작은 수*2)
-            int newNum = pque.poll() + (pque.poll() * 2);
+            int newNum = pque.poll() + pque.poll()*2;           
             pque.offer(newNum);
             answer++;
         }
         
-        if(!flag) answer = -1;
+        if(answer == scoville.length-1 && pque.peek() < K) answer = -1;
         return answer;
     }
 }
