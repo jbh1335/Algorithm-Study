1300cb6fc27ce539cbd5e356e4d6af4860e81fc1
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-25 23:12:53 +0900

    [level 3] Title: 정수 삼각형, Time: 12.10 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
-        int[][] dp = new int[triangle.length][triangle.length];
+        int N = triangle.length;
+        int[][] dp = new int[N][N];
         dp[0][0] = triangle[0][0];
         
-        for(int i = 0; i < triangle.length-1; i++) {
-            for(int j = 0; j < triangle[i].length; j++) {
-                dp[i+1][j] = Math.max(dp[i+1][j], dp[i][j]+triangle[i+1][j]);
-                dp[i+1][j+1] = Math.max(dp[i+1][j+1], dp[i][j]+triangle[i+1][j+1]);
-                
-                answer = Math.max(answer, Math.max(dp[i+1][j], dp[i+1][j+1]));
+        for(int i = 1; i < N; i++) {
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+            for(int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                answer = Math.max(answer, dp[i][j]);
             }
         }
         
