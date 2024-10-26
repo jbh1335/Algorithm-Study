7a169fc02be953994dfcefb107c895bb77f8e1d6
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-26 20:24:17 +0900

    [level 3] Title: 최고의 집합, Time: 0.46 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] solution(int n, int s) {
         int[] answer = new int[n];
+        int left = n;
         
-        int idx = 0;
-        while(n > 0) {
-            answer[idx] = s / n;
-            if(answer[idx] == 0) return new int[] {-1};
-            
-            s -= answer[idx++];
-            n--;
+        for(int i = 0; i < n; i++) {
+            answer[i] = s / left--;
+            if(answer[i] == 0) return new int[] {-1};
+            s -= answer[i];
         }
         
         return answer;
