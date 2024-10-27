ded29cfaf006a09177918a014dc78faf26f9a96a
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-10-27 19:54:48 +0900

    [level 3] Title: 단속카메라, Time: 10.46 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,23 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1;
-        Arrays.sort(routes, (arr1, arr2) -> {
-            if(arr1[0] == arr2[0]) return arr1[1] - arr2[1];
-            return arr1[0] - arr2[0];
-        });
+        int answer = 0;
+        int beforeEnd = -30001;
+        Arrays.sort(routes, (arr1, arr2) -> arr1[1] - arr2[1]);
         
-        int beforeEnd = routes[0][1];
-        for(int i = 1; i < routes.length; i++) {
-            int start = routes[i][0];
-            
-            if(beforeEnd < start) {
-                answer++;
-                beforeEnd = routes[i][1];
+        for(int[] route : routes) {
+            if(route[0] <= beforeEnd) {
+                continue;
             } else {
-                beforeEnd = Math.min(beforeEnd, routes[i][1]);
+                answer++;
+                beforeEnd = route[1];
             }
         }
+        
         return answer;
     }
 }
