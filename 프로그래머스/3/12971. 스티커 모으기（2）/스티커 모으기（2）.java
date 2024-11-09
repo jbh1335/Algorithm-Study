d5e7bd1af3d81b1170ae56f2594344f73ecc7c71
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-11-09 19:24:19 +0900

    [level 3] Title: 스티커 모으기(2), Time: 10.57 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -2,29 +2,31 @@ class Solution {
     public int solution(int sticker[]) {
         int answer = 0;
         if(sticker.length == 1) return sticker[0];
+        if(sticker.length == 2) return Math.max(sticker[0], sticker[1]);
         
         int[] dp = new int[sticker.length];
-        
-        // 0번째 스티커 뽑았을 때 -> 마지막번째 스티커는 뽑지 않음
         dp[0] = sticker[0];
-        dp[1] = dp[0];
-        for(int i = 2; i < sticker.length-1; i++) {
-            // 스티커를 뽑았을 경우와 뽑지 않을 경우 중 최댓값 구하기
-            // 뽑았을 경우: i-1번째는 뽑지 못하니 i-2번째까지의 최댓값 + 자기자신
-            // 뽑지 않을 경우: i-1까지의 최댓값
-            dp[i] = Math.max(dp[i-2]+sticker[i], dp[i-1]);
-        }      
-        answer = dp[sticker.length-2];
+        dp[1] = sticker[1];
+        dp[2] = dp[0] + sticker[2];
         
-        // 0번째 스티커를 뽑지 않았을 때 -> 마지막 스티커까지 검사
+        answer = Math.max(dp[1], dp[2]);
+        
+        // 0번째를 뽑았을 때 -> 마지막꺼는 뽑지 않음
+        for(int i = 3; i < sticker.length-1; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3]) + sticker[i];
+            answer = Math.max(answer, dp[i]);
+        }
+        
         dp = new int[sticker.length];
         dp[0] = 0;
         dp[1] = sticker[1];
-        for(int i = 2; i < sticker.length; i++) {
-            dp[i] = Math.max(dp[i-2]+sticker[i], dp[i-1]);
+        dp[2] = sticker[2];
+        // 0번째를 뽑지 않았을 때 -> 마지막꺼 뽑을 수 있음
+        for(int i = 3; i < sticker.length; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3]) + sticker[i];
+            answer = Math.max(answer, dp[i]);
         }
         
-        answer = Math.max(answer, dp[sticker.length-1]);
         return answer;
     }
 }
