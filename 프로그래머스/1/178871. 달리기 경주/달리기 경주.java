13ddcd965558069f4f4f6242976ff272df6fc019
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-11-24 21:57:48 +0900

    [level 1] Title: 달리기 경주, Time: 368.74 ms, Memory: 360 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,22 +1,23 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+        String[] answer = players;
         HashMap<String, Integer> map = new HashMap<>();
         for(int i = 0; i < players.length; i++) {
             map.put(players[i], i);
         }
         
-        for(int i = 0; i < callings.length; i++) {
-            int idx = map.get(callings[i]);
-            String frontPlayer = players[idx-1];
+        for(String call : callings) {
+            int idx = map.get(call);
+            String frontPlayer = answer[idx-1];
             
-            players[idx-1] = callings[i];
-            players[idx] = frontPlayer;
+            answer[idx-1] = answer[idx];
+            answer[idx] = frontPlayer;
             
-            map.replace(callings[i], idx-1);
+            map.replace(call, idx-1);
             map.replace(frontPlayer, idx);
         }
         
-        return players;
+        return answer;
     }
 }
