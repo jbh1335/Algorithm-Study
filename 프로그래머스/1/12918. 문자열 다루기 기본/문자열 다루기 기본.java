e3b183fc8ef84747e3c3db6978452ff1be8fd9ea
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-11-29 00:34:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = false;
+        
         if(s.length() == 4 || s.length() == 6) {
             String str = s.replaceAll("[0-9]", "");
-            if(str.length() == 0) answer = true;
+            if(str.equals("")) answer = true;
         }
+        
         return answer;
     }
 }
