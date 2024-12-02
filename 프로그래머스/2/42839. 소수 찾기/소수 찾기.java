49060c06838976a139d3312253da714bc92b5fb9
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-12-02 16:58:58 +0900

    [level 2] Title: 소수 찾기, Time: 29.22 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,49 +1,41 @@
 import java.util.*;
 class Solution {
-    static int answer;
-    static int[] select;
     static boolean[] visited;
-    static HashSet<Integer> set = new HashSet<>();
+    static HashSet<Integer> set;
     public int solution(String numbers) {
         visited = new boolean[numbers.length()];
+        set = new HashSet<>();
         
         for(int i = 1; i <= numbers.length(); i++) {
-            select = new int[i];
-            per(0, i, numbers);
+            per(0, i, "", numbers);
         }
-        return answer;
+        
+        return set.size();
     }
     
-    public static void per(int cnt, int size, String numbers) {
-        if(cnt == size) {
-            String num = "";
-            for(int i = 0; i < size; i++) {
-                num += select[i];
-            }
-            
-            if(checkOk(Integer.parseInt(num))) {
-                if(set.add(Integer.parseInt(num))) answer++;
-            }
+    public static void per(int cnt, int length, String str, String numbers) {
+        if(cnt == length) {
+            if(checkPrimeNumber(Integer.parseInt(str))) set.add(Integer.parseInt(str));
             return;
         }
         
         for(int i = 0; i < numbers.length(); i++) {
             if(visited[i]) continue;
             
-            select[cnt] = numbers.charAt(i) - '0';
             visited[i] = true;
-            per(cnt+1, size, numbers);
+            per(cnt+1, length, str+numbers.charAt(i), numbers);
             visited[i] = false;
         }
     }
     
-    public static boolean checkOk(int num) {
+    public static boolean checkPrimeNumber(int num) {
         if(num == 0 || num == 1) return false;
         
         int sqrt = (int) Math.sqrt(num);
         for(int i = 2; i <= sqrt; i++) {
             if(num % i == 0) return false;
         }
+        
         return true;
     }
 }
