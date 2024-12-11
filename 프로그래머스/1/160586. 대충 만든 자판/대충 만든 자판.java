bcdf454bd5f3368ecc3e5d491ea36c8939549a6a
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2024-12-11 21:09:22 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.85 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,28 +1,27 @@
+import java.util.*;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
+        int[] cntArr = new int[26];
+        Arrays.fill(cntArr, 101);
+        
+        for(String key : keymap) {
+            for(int i = 0; i < key.length(); i++) {
+                cntArr[key.charAt(i)-'A'] = Math.min(i+1, cntArr[key.charAt(i)-'A']);
+            }
+        }
         
         for(int i = 0; i < targets.length; i++) {
-            int count = 0;
-            boolean exist = true;
             for(int j = 0; j < targets[i].length(); j++) {
-                char ch = targets[i].charAt(j);
-                int minIdx = 100;
-                
-                for(int a = 0; a < keymap.length; a++) {
-                    int idx = keymap[a].indexOf(ch);
-                    if(idx != -1) minIdx = Math.min(minIdx, idx);
-                }
-                
-                if(minIdx == 100) {
-                    exist = false;
+                if(cntArr[targets[i].charAt(j)-'A'] == 101) {
+                    answer[i] = -1;
                     break;
-                } else {
-                    count += (minIdx+1);
                 }
+                
+                answer[i] += cntArr[targets[i].charAt(j)-'A'];
             }
-            answer[i] = exist ? count : -1;
         }
+        
         return answer;
     }
 }
