265bafc978275bf74f6119d3a379ae91bc3e8276
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-17 18:20:12 +0900

    [level 2] Title: 구명보트, Time: 9.71 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,18 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
+        int answer = 0, end = people.length - 1;
         Arrays.sort(people);
         
-        int idx = 0;
-        for(int i = people.length-1; i >= 0; i--) {
-            if(idx >= i) {
-                if(idx == i) answer++;
-                break;
+        for(int i = 0; i <= end; i++) {
+            for(int j = end; j > i; j--) {
+                end--;
+                if(people[i] + people[j] <= limit) break;
+                answer++;
             }
-            if(people[i] + people[idx] <= limit) idx++;
+            
             answer++;
         }
+        
         return answer;
     }
 }
