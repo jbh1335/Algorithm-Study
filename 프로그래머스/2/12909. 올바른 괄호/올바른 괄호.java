e5dc4ae16c8f4d38cb0301e6246a384e58426a8d
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-17 16:22:02 +0900

    [level 2] Title: 올바른 괄호, Time: 16.54 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,21 +2,21 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Stack<Character> stack = new Stack<>();
+        Stack<String> stack = new Stack<>();
         
         for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if(ch == '(') stack.push(ch);
+            if(s.charAt(i) == '(') stack.push("(");
             else {
-                if(stack.isEmpty()) {
+                if(!stack.isEmpty()) stack.pop();
+                else {
                     answer = false;
                     break;
                 }
-                stack.pop();
             }
         }
-        
+
         if(!stack.isEmpty()) answer = false;
+
         return answer;
     }
 }
