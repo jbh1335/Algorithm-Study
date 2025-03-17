01ce3deb72e0760b870ffe5b6e505621960d431c
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-17 16:50:20 +0900

    [level 2] Title: 숫자의 표현, Time: 1.33 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#) 
 
 ### 성능 요약
 
-메모리: 52.1 MB, 시간: 0.70 ms
+메모리: 54.3 MB, 시간: 1.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 19:28:32
+2025년 03월 17일 16:50:18
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
+        int answer = 0;
         
-        for(int i = 1; i <= n/2; i++) {
-            int sum = 0, num = i;
-            while(true) {
-                sum += num++;
+        for(int i = 1; i <= n; i++) {
+            int sum = 0;
+            for(int j = i; j <= n; j++) {
+                sum += j;
                 if(sum >= n) {
                     if(sum == n) answer++;
                     break;
