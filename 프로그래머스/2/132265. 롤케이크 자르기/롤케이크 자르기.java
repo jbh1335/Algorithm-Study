83876934a795e912ebf19dac50aa9c57b831bbf9
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 22:49:28 +0900

    [level 2] Title: 롤케이크 자르기, Time: 267.96 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,27 +2,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        HashMap<Integer, Integer> leftMap = new HashMap<>(); // 형
-        HashMap<Integer, Integer> rightMap = new HashMap<>(); // 동생
         
-        leftMap.put(topping[0], 1);
-        for(int i = 1; i < topping.length; i++) {
+        HashMap<Integer, Integer> leftMap = new HashMap<>();
+        HashMap<Integer, Integer> rightMap = new HashMap<>();
+        
+        for(int i = 0; i < topping.length; i++) {
             rightMap.put(topping[i], rightMap.getOrDefault(topping[i], 0) + 1);
         }
         
-        if(leftMap.size() == rightMap.size()) answer++;
-        
-        for(int i = 1; i < topping.length; i++) {
-            int num = topping[i];
-            // 형 추가하고
-            leftMap.put(num, leftMap.getOrDefault(num, 0) + 1);
+        for(int i = 0; i < topping.length; i++) {
+            int type = topping[i];
             
-            // 동생은 하나 빼주기
-            if(rightMap.get(num) == 1) rightMap.remove(num);
-            else rightMap.put(num, rightMap.getOrDefault(num, 0) - 1);
+            leftMap.put(type, leftMap.getOrDefault(type, 0) + 1);
+            if(rightMap.get(type) == 1) rightMap.remove(type);
+            else rightMap.put(type, rightMap.get(type) - 1);
             
             if(leftMap.size() == rightMap.size()) answer++;
+            if(leftMap.size() > rightMap.size()) break;
         }
+        
         return answer;
     }
 }
