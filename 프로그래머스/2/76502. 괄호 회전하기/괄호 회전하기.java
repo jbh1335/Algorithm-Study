86d5191ad6d9496888114c4ee1b5a698fc24d716
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 18:40:21 +0900

    [level 2] Title: 괄호 회전하기, Time: 51.48 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,32 +4,34 @@ class Solution {
         int answer = 0;
         
         for(int i = 0; i < s.length(); i++) {
-            String str = s.substring(i, s.length()) + s.substring(0, i);
             Stack<Character> stack = new Stack<>();
+            String str = s.substring(i, s.length()) + s.substring(0, i);
             boolean isAble = true;
             
             for(int j = 0; j < str.length(); j++) {
                 char ch = str.charAt(j);
-                if(ch == '[' || ch == '(' || ch == '{') {
-                    stack.push(ch);
-                } else {
-                    if(!stack.isEmpty()) {
-                        char before = stack.peek();
-                        
-                        if(ch == ']') {
-                            if(before != '[') isAble = false;
-                        } else if(ch == ')') {
-                            if(before != '(') isAble = false;
-                        } else {
-                            if(before != '{') isAble = false;
-                        }
-                    } else {
+                
+                if(ch == ')' || ch == ']' || ch == '}') {
+                    if(stack.isEmpty()) {
                         isAble = false;
+                        break;
                     }
                     
-                    if(isAble) stack.pop();
-                    else break;
+                    if(ch == ')') {
+                        if(stack.peek() == '(') stack.pop();
+                        else isAble = false;
+                    } else if(ch == ']') {
+                        if(stack.peek() == '[') stack.pop();
+                        else isAble = false;
+                    } else if(ch == '}') {
+                        if(stack.peek() == '{') stack.pop();
+                        else isAble = false;
+                    }
+                } else {
+                    stack.push(ch);
                 }
+                
+                if(!isAble) break;
             }
             
             if(isAble && stack.isEmpty()) answer++;
