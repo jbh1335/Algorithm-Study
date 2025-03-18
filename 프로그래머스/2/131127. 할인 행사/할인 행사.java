840c992eb3b0ccc5db6a01a2a8aa500277c20d97
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 19:01:17 +0900

    [level 2] Title: 할인 행사, Time: 34.25 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,34 +1,30 @@
 import java.util.*;
 class Solution {
-    public static HashMap<String, Integer> wantMap, buyMap;
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
         
-        wantMap = new HashMap<>();
-        for(int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
-        
-        buyMap = new HashMap<>();
         for(int i = 0; i < 10; i++) {
-            buyMap.put(discount[i], buyMap.getOrDefault(discount[i], 0)+1);
+            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
         }
         
-        if(checkOk()) answer++;    
-        for(int i = 10; i < discount.length; i++) {
-            buyMap.put(discount[i], buyMap.getOrDefault(discount[i], 0)+1);
-            buyMap.put(discount[i-10], buyMap.get(discount[i-10])-1);
+        if(checkOk(map, want, number)) answer++;
+        
+        for(int i = 1; i <= discount.length-10; i++) {
+            map.put(discount[i-1], map.getOrDefault(discount[i-1], 0) - 1);
+            map.put(discount[i+9], map.getOrDefault(discount[i+9], 0) + 1);
             
-            if(checkOk()) answer++;
+            if(checkOk(map, want, number)) answer++;
         }
+        
         return answer;
     }
     
-    public static boolean checkOk() {
-        for(String s : wantMap.keySet()) {
-            if(!buyMap.containsKey(s)) return false;
-            if(wantMap.get(s) > buyMap.get(s)) return false;
+    public static boolean checkOk(HashMap<String, Integer> map, String[] want, int[] number) {
+        for(int i = 0; i < want.length; i++) {
+            if(map.getOrDefault(want[i], 0) < number[i]) return false;
         }
+        
         return true;
     }
 }
