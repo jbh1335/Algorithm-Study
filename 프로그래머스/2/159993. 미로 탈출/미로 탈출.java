467a3a9c27ffda67752d4d2faa2e25c1157ac63f
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 23:43:02 +0900

    [level 2] Title: 미로 탈출, Time: 8.77 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,9 +1,6 @@
 import java.util.*;
 class Solution {
     static int N, M;
-    static char[][] map;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
     static class Point {
         int x, y, dist;
         public Point(int x, int y, int dist) {
@@ -13,44 +10,46 @@ class Solution {
         }
     }
     public int solution(String[] maps) {
+        int answer = -1;
         N = maps.length;
         M = maps[0].length();
-        map = new char[N][M];
         
         int startX = 0, startY = 0, leverX = 0, leverY = 0;
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
-                map[i][j] = maps[i].charAt(j);
-                // 시작 위치
-                if(map[i][j] == 'S') {
+                if(maps[i].charAt(j) == 'S') {
                     startX = i;
                     startY = j;
                 }
                 
-                // 레버 위치
-                if(map[i][j] == 'L') {
+                if(maps[i].charAt(j) == 'L') {
                     leverX = i;
                     leverY = j;
                 }
             }
         }
         
-        // 레버 찾기
-        int answer = bfs(startX, startY, 'L');
-        // 레버를 찾았으면 출구 찾기
-        if(answer != -1) {
-            int exit = bfs(leverX, leverY, 'E');
-            answer = exit == -1 ? -1 : answer + exit;
+        // 레버 찾으러 가는 거리
+        int leverDist = bfs(startX, startY, 'L', maps);
+        
+        // 출구 가는 거리
+        if(leverDist != -1) {
+            int exitDist = bfs(leverX, leverY, 'E', maps);
+            if(exitDist != -1) answer = leverDist + exitDist;
         }
         
         return answer;
     }
     
-    public static int bfs(int x, int y, char target) {
+    public static int bfs(int x, int y, char target, String[] maps) {
         Queue<Point> que = new LinkedList<>();
         que.offer(new Point(x, y, 0));
+        
         boolean[][] visited = new boolean[N][M];
         visited[x][y] = true;
+        
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         
         while(!que.isEmpty()) {
             Point p = que.poll();
@@ -59,9 +58,10 @@ class Solution {
                 int nx = p.x + dx[d];
                 int ny = p.y + dy[d];
                 
-                if(nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny]) {
-                    if(map[nx][ny] == target) return p.dist + 1;
-                    if(map[nx][ny] != 'X') {
+                if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if(maps[nx].charAt(ny) == target) return p.dist + 1;
+                    
+                    if(!visited[nx][ny] && maps[nx].charAt(ny) != 'X') {
                         que.offer(new Point(nx, ny, p.dist+1));
                         visited[nx][ny] = true;
                     }
