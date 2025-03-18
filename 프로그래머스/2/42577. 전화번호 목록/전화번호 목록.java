f9365e72dea5b4a751711ec16c55d6628a829f9b
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 21:46:42 +0900

    [level 2] Title: 전화번호 목록, Time: 351.66 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,13 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        for(int i = 0; i < phone_book.length; i++) {
-            phone_book[i] = phone_book[i].replace(" ", "");
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        
+        for(int i = 1; i < phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])) return false;
         }
-        Arrays.sort(phone_book);     
-        for(int i = 0; i < phone_book.length-1; i++) {
-            String start = phone_book[i];
-            if(phone_book[i+1].startsWith(start)) return false;
-        }
-        return true;
+        
+        return answer;
     }
 }
