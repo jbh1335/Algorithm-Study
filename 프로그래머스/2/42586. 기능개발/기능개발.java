c30be7d1a72267fd288ffbe34f3f9fbaf43f742a
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 20:58:46 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,32 +2,30 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> list = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        int count = 0;
-        for(int i = 0; i < progresses.length; i++) {
+        int before = (100 - progresses[0]) / speeds[0];
+        if((100 - progresses[0]) % speeds[0] != 0) before++;
+        int count = 1;
+        
+        for(int i = 1; i < progresses.length; i++) {
             int day = (100 - progresses[i]) / speeds[i];
             if((100 - progresses[i]) % speeds[i] != 0) day++;
             
-            if(stack.isEmpty()) {
-                stack.push(day);
-                count++;
-                continue;
+            if(before < day) {
+                list.add(count);
+                count = 0;
+                before = day;
             }
             
-            if(day <= stack.peek()) count++;
-            else {
-                stack.pop();
-                stack.push(day);
-                list.add(count);
-                count = 1;
-            }
+            count++;
         }
         
         list.add(count);
         int[] answer = new int[list.size()];
+        
         for(int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
