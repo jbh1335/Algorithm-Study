162c0ccae0418ef812f91ec06c333cf8421cc66e
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-18 18:03:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,23 +2,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
+        char before = words[0].charAt(words[0].length() - 1);
+        
+        HashSet<String> set = new HashSet<>();
+        set.add(words[0]);
 
-        char beforeLastCh = words[0].charAt(words[0].length()-1);
-        ArrayList<String> list = new ArrayList<>();
-        list.add(words[0]);
         for(int i = 1; i < words.length; i++) {
-            String str = words[i];
-            
-            if(list.contains(str) || beforeLastCh != str.charAt(0)) {
+            if(!set.add(words[i]) || words[i].charAt(0) != before) {
                 answer[0] = i % n + 1;
                 answer[1] = i / n + 1;
                 break;
             }
             
-            beforeLastCh = str.charAt(str.length()-1);
-            list.add(str);
+            before = words[i].charAt(words[i].length() - 1);
         }
-
+        
         return answer;
     }
 }
