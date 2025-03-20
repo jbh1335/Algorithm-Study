0a7fccafd32a7e5ee84020ccfecb93298b3e85f4
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-20 19:53:56 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.65 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        String changedNum = Integer.toString(n, k);
-        String[] strArr = changedNum.split("0+");
+        String[] splitArr = Integer.toString(n, k).split("0+");
         
-        for(String s : strArr) {
-            if(checkOk(Long.valueOf(s))) answer++;
+        for(String str : splitArr) {
+            if(checkOk(Long.valueOf(str))) answer++;
         }
         
         return answer;
@@ -13,11 +12,13 @@ class Solution {
     
     public static boolean checkOk(long num) {
         if(num == 1) return false;
-        int sqrt = (int) Math.sqrt(num);
+        if(num == 2) return true;
         
+        int sqrt = (int) Math.sqrt(num);
         for(int i = 2; i <= sqrt; i++) {
             if(num % i == 0) return false;
         }
+        
         return true;
     }
 }
