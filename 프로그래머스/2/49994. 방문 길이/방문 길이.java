4e1235aa24252339c6495cc765490346875b5fac
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-03-20 18:57:00 +0900

    [level 2] Title: 방문 길이, Time: 23.44 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,36 +1,26 @@
 import java.util.*;
 class Solution {
     public int solution(String dirs) {
-        int answer = 0;
-        ArrayList<String> list = new ArrayList<>();
-        
+        HashSet<String> set = new HashSet<>();
         int x = 0, y = 0;
+        
         for(int i = 0; i < dirs.length(); i++) {
-            char ch = dirs.charAt(i);
-            int nx = x;
-            int ny = y;
+            char dir = dirs.charAt(i);
+            int nx = x, ny = y;
             
-            if(ch == 'U') {
-                nx++;
-            } else if(ch == 'D') {
-                nx--;
-            } else if(ch == 'R') {
-                ny++;
-            } else if(ch == 'L') {
-                ny--;
-            }
+            if(dir == 'U') ny = y + 1;
+            else if(dir == 'D') ny = y - 1;
+            else if(dir == 'L') nx = x - 1;
+            else nx = x + 1;
             
             if(nx >= -5 && ny >= -5 && nx <= 5 && ny <= 5) {
-                String str1 = "" + x + y + nx + ny;
-                String str2 = "" + nx + ny + x + y;
-                if(!list.contains(str1) && !list.contains(str2)) {
-                    answer++;
-                    list.add(str1);
-                }
+                set.add("" + x + y + nx + ny);
+                set.add("" + nx + ny + x + y);
                 x = nx;
                 y = ny;
             }
         }
-        return answer;
+        
+        return set.size() / 2;
     }
 }
