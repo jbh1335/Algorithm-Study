bfc52119b417213c03bf28f9a6810018dd63f93e
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-23 19:17:56 +0900

    [level 2] Title: 숫자 변환하기, Time: 91.65 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,39 +1,39 @@
 import java.util.*;
 class Solution {
-    static class Point {
-        int num, cnt;
-        public Point(int num, int cnt) {
-            this.num = num;
-            this.cnt = cnt;
-        }
-    }
     public int solution(int x, int y, int n) {
-        return bfs(x, y, n);
+        int answer = bfs(x, y, n);
+        return answer;
     }
     
     public static int bfs(int x, int y, int n) {
-        Queue<Point> que = new LinkedList<>();
-        que.offer(new Point(x, 0));
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(x);
+        int count = 0;
         boolean[] visited = new boolean[y+1];
         
         while(!que.isEmpty()) {
-            Point p = que.poll();
-            if(p.num == y) return p.cnt;
-            
-            if(p.num+n <= y && !visited[p.num+n]) {
-                que.offer(new Point(p.num+n, p.cnt+1));
-                visited[p.num+n] = true;
+            int size = que.size();
+            for(int s = 0; s < size; s++) {
+                int num = que.poll();
+                if(num == y) return count;
+                
+                if((num + n) <= y && !visited[num + n]) {
+                    que.offer(num + n);
+                    visited[num + n] = true;
+                }
+                
+                if((num * 2) <= y && !visited[num * 2]) {
+                    que.offer(num * 2);
+                    visited[num * 2] = true;
+                }
+                
+                if((num * 3) <= y && !visited[num * 3]) {
+                    que.offer(num * 3);
+                    visited[num * 3] = true;
+                }
             }
             
-            if(p.num*2 <= y && !visited[p.num*2]) {
-                que.offer(new Point(p.num*2, p.cnt+1));
-                visited[p.num*2] = true;
-            }
-            
-            if(p.num*3 <= y && !visited[p.num*3]) {
-                que.offer(new Point(p.num*3, p.cnt+1));
-                visited[p.num*3] = true;
-            }
+            count++;
         }
         
         return -1;
