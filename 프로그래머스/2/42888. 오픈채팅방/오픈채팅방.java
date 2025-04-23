b8a0ba4e5924b6419253fdb1b293874bee7bade7
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-23 20:54:19 +0900

    [level 2] Title: 오픈채팅방, Time: 182.77 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,31 +1,28 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
-        // 유저아이디, 닉네임을 저장하는 userMap
-        HashMap<String, String> userMap = new HashMap<>();
-        // 유저아이디, 채팅 기록 저장하는 resultList
-        ArrayList<String[]> resultList = new ArrayList<>();
+        HashMap<String, String> userIdMap = new HashMap<>();
+        ArrayList<String[]> list = new ArrayList<>();
         
-        for(int i = 0; i < record.length; i++) {
-            String[] recordArr = record[i].split(" ");
+        for(String str : record) {
+            String[] splitArr = str.split(" ");
             
-            if(recordArr[0].equals("Enter")) { // 입장
-                // 유저아이디와 들어온 정보 저장
-                resultList.add(new String[] {recordArr[1], "님이 들어왔습니다."});
-                // 유저아이디의 닉네임 저장
-                userMap.put(recordArr[1], recordArr[2]);
-            } else if(recordArr[0].equals("Leave")) { // 퇴장
-                resultList.add(new String[] {recordArr[1], "님이 나갔습니다."});
-            } else { // 닉네임 변경
-                userMap.put(recordArr[1], recordArr[2]);
+            if(splitArr[0].equals("Enter")) {
+                userIdMap.put(splitArr[1], splitArr[2]);
+                list.add(new String[] {splitArr[1], "님이 들어왔습니다."});
+            } else if(splitArr[0].equals("Leave")) {
+                list.add(new String[] {splitArr[1], "님이 나갔습니다."});
+            } else {
+                userIdMap.put(splitArr[1], splitArr[2]);
             }
         }
         
-        String[] answer = new String[resultList.size()];
+        String[] answer = new String[list.size()];
         for(int i = 0; i < answer.length; i++) {
-            String[] sArr = resultList.get(i);
-            answer[i] = userMap.get(sArr[0]) + sArr[1];
+            String[] arr = list.get(i);
+            answer[i] = userIdMap.get(arr[0]) + arr[1];
         }
+        
         return answer;
     }
 }
