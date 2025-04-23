38e3ec7d5702bf19d6d9995a03f7d4bd4feeb141
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-23 16:36:21 +0900

    [level 2] Title: 땅따먹기, Time: 24.15 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,24 +1,22 @@
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
-        int[][] distance = new int[land.length][4];
-        
+        int[][] dist = new int[land.length][4];
         for(int j = 0; j < 4; j++) {
-            distance[0][j] = land[0][j];
+            dist[0][j] = land[0][j];
+            answer = Math.max(answer, dist[0][j]);
         }
         
-        int idx = 1;
-        while(true) {
-            for(int i = 0; i < 4; i++) {
-                for(int j = 0; j < 4; j++) {
-                    if(i == j) continue;
-                    distance[idx][j] = Math.max(distance[idx][j], distance[idx-1][i] + land[idx][j]);
-                    answer = Math.max(answer, distance[idx][j]);
+        for(int i = 1; i < land.length; i++) {
+            for(int j = 0; j < 4; j++) {
+                for(int k = 0; k < 4; k++) {
+                    if(j == k) continue;
+                    dist[i][j] = Math.max(dist[i][j], land[i][j]+dist[i-1][k]);
+                    answer = Math.max(answer, dist[i][j]);
                 }
             }
-            if(++idx == land.length) break;
         }
-        
+
         return answer;
     }
 }
