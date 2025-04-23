5c5e264c2ac1bc495e6160090f7939d926d8d6d4
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-23 21:45:24 +0900

    [level 2] Title: 소수 찾기, Time: 22.30 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,8 +3,8 @@ class Solution {
     static boolean[] visited;
     static HashSet<Integer> set;
     public int solution(String numbers) {
-        visited = new boolean[numbers.length()];
         set = new HashSet<>();
+        visited = new boolean[numbers.length()];
         
         for(int i = 1; i <= numbers.length(); i++) {
             per(0, i, "", numbers);
@@ -13,9 +13,10 @@ class Solution {
         return set.size();
     }
     
-    public static void per(int cnt, int length, String str, String numbers) {
-        if(cnt == length) {
-            if(checkPrimeNumber(Integer.parseInt(str))) set.add(Integer.parseInt(str));
+    public static void per(int cnt, int N, String str, String numbers) {
+        if(cnt == N) {
+            int num = Integer.parseInt(str);
+            if(checkPrimeNumber(num)) set.add(num);
             return;
         }
         
@@ -23,15 +24,15 @@ class Solution {
             if(visited[i]) continue;
             
             visited[i] = true;
-            per(cnt+1, length, str+numbers.charAt(i), numbers);
+            per(cnt+1, N, str+numbers.charAt(i), numbers);
             visited[i] = false;
         }
     }
     
     public static boolean checkPrimeNumber(int num) {
         if(num == 0 || num == 1) return false;
-        
         int sqrt = (int) Math.sqrt(num);
+        
         for(int i = 2; i <= sqrt; i++) {
             if(num % i == 0) return false;
         }
