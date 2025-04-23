cbc52a045254447eca8962d0b2d929eabd567bfd
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-23 18:55:07 +0900

    [level 2] Title: 주차 요금 계산, Time: 7.36 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,69 +1,59 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] fees, String[] records) { 
-        // 차량이 소요한 시간을 구하기 위한 recordMap
-        HashMap<String, Integer> recordMap = new HashMap<>();
-        // 차량마다 총 소요한 시간을 저장하는 timeMap
-        HashMap<String, Integer> timeMap = new HashMap<>();
-        for(int i = 0; i < records.length; i++) {
-            // 시각, 차량번호, 내역을 구분
-            String[] recordArr = records[i].split(" ");
-            String carNum = recordArr[1];
+    public int[] solution(int[] fees, String[] records) {
+        HashMap<Integer, Integer> timeMap = new HashMap<>(); // 차량의 입차 시간 저장
+        HashMap<Integer, Integer> totalTimeMap = new HashMap<>(); // 차량의 누적 주차 시간 저장
+        
+        for(String record : records) {
+            String[] splitRecord = record.split(" ");
             
-            // 시각을 시간과 분으로 구분
-            String[] timeArr = recordArr[0].split(":");
-            int hour = Integer.parseInt(timeArr[0]);
-            int min = Integer.parseInt(timeArr[1]);
+            // 입/출차 시간
+            String[] splitTime = splitRecord[0].split(":");
+            int time = Integer.parseInt(splitTime[0]) * 60 + Integer.parseInt(splitTime[1]);
             
-            // 시간을 분으로 변경해서 계산
-            int totalMin = (hour * 60) + min;
+            // 차량번호
+            int carNum = Integer.parseInt(splitRecord[1]);
             
-            if(recordArr[2].equals("IN")) { // 입차           
-                recordMap.put(recordArr[1], totalMin);
+            if(splitRecord[2].equals("IN")) { // 입차
+                timeMap.put(carNum, time); // 들어간 시간 저장
             } else { // 출차
-                // 몇분동안 있었는지 구하기
-                int time = totalMin - recordMap.get(carNum);
-                timeMap.put(carNum, timeMap.getOrDefault(carNum, 0) + time);
-                recordMap.remove(carNum);
+                int stayedTime = time - timeMap.get(carNum); // 머문 시간
+                totalTimeMap.put(carNum, totalTimeMap.getOrDefault(carNum, 0) + stayedTime); // 누적 시간 저장
+                timeMap.remove(carNum);
             }
         }
         
-        // 입차기록은 있지만 출차기록이 없는 경우 -> 23:59에 출차한 것으로 계산
-        for(String s : recordMap.keySet()) {
-            int outTime = (60 * 23) + 59;
-            int time = outTime - recordMap.get(s);
-            
-            timeMap.put(s, timeMap.getOrDefault(s, 0) + time);
+        // 입차는 있고 출차 내역이 없으면 23:59에 출차되었다고 간주
+        for(int carNum : timeMap.keySet()) {
+            int lastTime = 23 * 60 + 59;
+            int stayedTime = lastTime - timeMap.get(carNum);
+            totalTimeMap.put(carNum, totalTimeMap.getOrDefault(carNum, 0) + stayedTime);
         }
         
-        // 요금 계산
-        for(String s : timeMap.keySet()) {
-            // 총 소요시간
-            int time = timeMap.get(s);
-            // 기본요금에 대한 시간 빼고
-            time -= fees[0]; 
-            // 기본요금 부여
-            int fee = fees[1];
+        // 각 차량마다 누적 시간으로 요금 계산
+        for(int carNum : totalTimeMap.keySet()) {
+            int time = totalTimeMap.get(carNum);
+            time -= fees[0]; // 기본 시간
+            int fee = fees[1]; // 기본 요금
             
-            // 시간이 남았으면
+            // 기본 요금으로 처리 못하면 추가 계산
             if(time > 0) {
-                // 남은 시간은 분당 요금으로 계산
                 fee += (time / fees[2]) * fees[3];
                 if(time % fees[2] != 0) fee += fees[3];
             }
             
-            // 요금으로 다시 저장
-            timeMap.put(s, fee);
+            totalTimeMap.put(carNum, fee);
         }
         
-        // 차량 번호에 따라 오름차순 정렬
-        ArrayList<String> list = new ArrayList<>(timeMap.keySet());
+        // 차량 번호 오름차순 정렬
+        ArrayList<Integer> list = new ArrayList<>(totalTimeMap.keySet());
         Collections.sort(list);
         
-        int[] answer = new int[timeMap.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = timeMap.get(list.get(i));
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = totalTimeMap.get(list.get(i));
         }
+        
         return answer;
     }
 }
