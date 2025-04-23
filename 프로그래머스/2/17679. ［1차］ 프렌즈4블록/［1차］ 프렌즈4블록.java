2b6ea6a2c54bede9f9d50d8c4d8fb16e49f18497
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-23 22:25:59 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 36.16 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -4,8 +4,8 @@ class Solution {
     static boolean[][] visited;
     public int solution(int m, int n, String[] board) {
         int answer = 0;
-        // board를 이차원 배열 map으로 만들기
         map = new char[m][n];
+        
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 map[i][j] = board[i].charAt(j);
@@ -14,64 +14,56 @@ class Solution {
         
         while(true) {
             visited = new boolean[m][n];
-            boolean flag = false;
-            // 2*2모양으로 같은 애들이 붙어있는지 찾기
+            // 같은 모양의 2*2 찾기
             for(int i = 0; i < m-1; i++) {
                 for(int j = 0; j < n-1; j++) {
                     char ch = map[i][j];
-                    if(ch == '.') continue;
-                    if(ch == map[i+1][j] && ch == map[i][j+1] && ch == map[i+1][j+1]) {
-                        visited[i][j] = visited[i+1][j] = visited[i][j+1] = visited[i+1][j+1] = true;
-                        flag = true;
+                    
+                    if(ch == 'X') continue;
+                    if(map[i][j+1] == ch && map[i+1][j] == ch && map[i+1][j+1] == ch) {
+                        visited[i][j] = visited[i][j+1] = visited[i+1][j] = visited[i+1][j+1] = true;
                     }
                 }
             }
             
-            // 더이상 찾을 것이 없으면 끝내기
-            if(!flag) break;
-            // 다 찾고 나서 삭제된 애들은 개수 구하고
-            answer += countDeleted();
-            // 위에서부터 내리기
-            drop();
+            // 지워진 블록 수
+            int count = deleteBlock(m, n);
+            if(count == 0) break;
+            answer += count;
+            
+            // 지워진 블록은 아래로 떨어짐
+            fallBlock(m, n);
         }
         
         return answer;
     }
     
-    // 삭제된 곳에 위에서부터 떨어뜨리기
-    public static void drop() {
-        for(int j = 0; j < map[0].length; j++) {
-            for(int i = map.length-1; i >= 0; i--) {
-                if(visited[i][j]) {
-                    int idx = i;
-                    // 위로 방문한 적 없는 가장 가까운 아이 찾기
-                    while(--idx >= 0) {
-                        if(map[idx][j] != '.') {
-                            // 빈곳으로 내려옴
-                            map[i][j] = map[idx][j];
-                            // 원래 있던 곳은 빈곳으로 변경
-                            map[idx][j] = '.';
-                            visited[idx][j] = true;
-                            break;
-                        }
-                    }
+    public static void fallBlock(int m, int n) {
+        Queue<Character> que = new LinkedList<>();
+        
+        for(int j = 0; j < n; j++) {
+            for(int i = m-1; i >= 0; i--) {
+                if(!visited[i][j]) {
+                    que.offer(map[i][j]);
                 }
+            }
+            
+            for(int i = m-1; i >= 0; i--) {
+                if(!que.isEmpty()) map[i][j] = que.poll();
+                else map[i][j] = 'X';
             }
         }
     }
     
-    // 삭제된 애들 개수 세주기
-    public static int countDeleted() {
+    public static int deleteBlock(int m, int n) {
         int count = 0;
-        for(int i = 0; i < visited.length; i++) {
-            for(int j = 0; j < visited[0].length; j++) {
-                if(visited[i][j]) {
-                    count++;
-                    // 빈칸으로 변경
-                    map[i][j] = '.';
-                }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(visited[i][j]) count++;
             }
         }
+        
         return count;
     }
 }
