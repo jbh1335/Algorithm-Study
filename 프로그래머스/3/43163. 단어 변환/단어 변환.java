c3e70ff0ef5459dca430372ea9abfd53319135d3
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-25 23:55:44 +0900

    [level 3] Title: 단어 변환, Time: 0.66 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -4,14 +4,16 @@ class Solution {
         String word;
         int cnt;
         boolean[] visited;
+        
         public Point(String word, int cnt, boolean[] visited) {
-            this.word = word;
             this.cnt = cnt;
             this.visited = visited;
+            this.word = word;
         }
     }
     public int solution(String begin, String target, String[] words) {
-        return bfs(begin, target, words);
+        int answer = bfs(begin, target, words);
+        return answer;
     }
     
     public static int bfs(String begin, String target, String[] words) {
@@ -22,11 +24,13 @@ class Solution {
             Point p = que.poll();
             
             for(int i = 0; i < words.length; i++) {
-                if(!p.visited[i] && isOneDiff(p.word, words[i])) {
-                    if(words[i].equals(target)) return p.cnt+1;
+                if(!p.visited[i] && checkOneDifferent(p.word, words[i])) {
+                    if(words[i].equals(target)) return p.cnt + 1;
                     
-                    p.visited[i] = true;
-                    que.offer(new Point(words[i], p.cnt+1, p.visited));
+                    boolean[] visited = new boolean[words.length];
+                    visited = p.visited.clone();
+                    visited[i] = true;
+                    que.offer(new Point(words[i], p.cnt+1, visited));
                 }
             }
         }
@@ -34,7 +38,7 @@ class Solution {
         return 0;
     }
     
-    public static boolean isOneDiff(String str1, String str2) {
+    public static boolean checkOneDifferent(String str1, String str2) {
         int count = 0;
         
         for(int i = 0; i < str1.length(); i++) {
