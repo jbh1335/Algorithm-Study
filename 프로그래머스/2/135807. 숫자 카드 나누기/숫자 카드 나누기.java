a3f10e263e7408e4c08ab9a0a561123338c5e5c2
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-25 18:36:50 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 52.95 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,36 +1,46 @@
+import java.util.*;
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        // arrayA의 최대공약수
-        int gcdA = arrayA[0];
+        
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+        
+        int gcdNumA = arrayA[0];
         for(int i = 1; i < arrayA.length; i++) {
-            gcdA = gcd(gcdA, arrayA[i]);
+            gcdNumA = gcd(gcdNumA, arrayA[i]);
         }
         
-        // arrayB의 최대공약수
-        int gcdB = arrayB[0];
+        int gcdNumB = arrayB[0];
         for(int i = 1; i < arrayB.length; i++) {
-            gcdB = gcd(gcdB, arrayB[i]);
+            gcdNumB = gcd(gcdNumB, arrayB[i]);
         }
         
-        // gcdA가 arrayB에 나누어 떨어지는지 확인
-        if(checkOk(gcdA, arrayB)) answer = Math.max(answer, gcdA);
+        boolean isAble = true;
+        for(int i : arrayA) {
+            if(i % gcdNumB == 0) {
+                isAble = false;
+                break;
+            }
+        }
         
-        // gcdB가 arrayA에 나누어 떨어지는지 확인
-        if(checkOk(gcdB, arrayA)) answer = Math.max(answer, gcdB);
+        if(isAble) answer = gcdNumB;
+        
+        isAble = true;
+        for(int i : arrayB) {
+            if(i % gcdNumA == 0) {
+                isAble = false;
+                break;
+            }
+        }
+        
+        if(isAble) answer = Math.max(answer, gcdNumA);
+        
         return answer;
     }
     
     public static int gcd(int a, int b) {
         if(b == 0) return a;
-        return gcd(b, a%b);
-    }
-    
-    // 해당 배열에서 나누어 떨어지는 수가 있는지 확인
-    public static boolean checkOk(int num, int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] % num == 0) return false;
-        }
-        return true;
+        return gcd(b, a % b);
     }
 }
