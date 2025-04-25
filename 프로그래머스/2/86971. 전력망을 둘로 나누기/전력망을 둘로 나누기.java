70cdd3c8a295fe1ff5a8cc6d52c9af5d1a6c5e0d
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-25 18:12:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 15.58 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,35 +1,28 @@
 import java.util.*;
 class Solution {
-    static int[][] connect;
-    static boolean[] visited;
+    static boolean[][] connect;
     public int solution(int n, int[][] wires) {
         int answer = n;
-        connect = new int[n+1][n+1];
+        connect = new boolean[n+1][n+1];
         
-        for(int i = 0; i < wires.length; i++) {
-            int a = wires[i][0];
-            int b = wires[i][1];
-            connect[a][b] = connect[b][a] = 1;
+        for(int[] arr : wires) {
+            connect[arr[0]][arr[1]] = connect[arr[1]][arr[0]] = true;
         }
         
-        // 하나씩 끊어보고 송전탑 개수의 차이 구하기
-        for(int i = 0; i < wires.length; i++) {
-            visited = new boolean[n+1];
-            int a = wires[i][0];
-            int b = wires[i][1];
-            connect[a][b] = connect[b][a] = 0;
-            
-            int count1 = bfs(1, n);
-            int count2 = n - count1;
-            answer = Math.min(answer, Math.abs(count1 - count2));
-            connect[a][b] = connect[b][a] = 1;
+        for(int[] arr : wires) {
+            connect[arr[0]][arr[1]] = connect[arr[1]][arr[0]] = false;
+            int count = bfs(1, n);
+            answer = Math.min(answer, Math.abs(n - count - count));
+            connect[arr[0]][arr[1]] = connect[arr[1]][arr[0]] = true;
         }
+        
         return answer;
     }
     
     public static int bfs(int x, int n) {
         Queue<Integer> que = new LinkedList<>();
         que.offer(x);
+        boolean[] visited = new boolean[n+1];
         visited[x] = true;
         int count = 1;
         
@@ -37,7 +30,7 @@ class Solution {
             int num = que.poll();
             
             for(int i = 1; i <= n; i++) {
-                if(!visited[i] && connect[num][i] == 1) {
+                if(!visited[i] && connect[num][i]) {
                     que.offer(i);
                     visited[i] = true;
                     count++;
