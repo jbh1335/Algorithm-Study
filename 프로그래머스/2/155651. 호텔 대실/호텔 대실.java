6e5a5304ed6f27eafb0c52c3695113802a349149
jbh1335 <119313897+jbh1335@users.noreply.github.com>
2025-04-25 18:58:09 +0900

    [level 2] Title: 호텔 대실, Time: 41.47 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,46 +1,27 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 0;
-        // 시간을 분으로 변경하여 int형으로 예약 정보 저장
-        int[][] bookArr = new int[book_time.length][2];
+        PriorityQueue<Integer> pque = new PriorityQueue<>();
+        int[][] bookTime = new int[book_time.length][2];
         
         for(int i = 0; i < book_time.length; i++) {
-            for(int j = 0; j < 2; j++) {
-                String[] splitArr = book_time[i][j].split(":");
-                // 시간을 분으로 변경
-                int hour = Integer.parseInt(splitArr[0]);
-                int min = Integer.parseInt(splitArr[1]);
-                min += hour * 60;
-                
-                // 대실 종료 시각은 청소 시간 10분 포함해서 저장
-                if(j == 1) min += 10;
-                bookArr[i][j] = min;
-            }
+            String[] startArr = book_time[i][0].split(":");
+            String[] endArr = book_time[i][1].split(":");
+            
+            bookTime[i][0] = Integer.parseInt(startArr[0]) * 60 + Integer.parseInt(startArr[1]);
+            bookTime[i][1] = Integer.parseInt(endArr[0]) * 60 + Integer.parseInt(endArr[1]);
         }
         
-        // 대실 시작 시간을 기준으로 오름차순 정렬
-        // 시작 시간이 같다면 종료 시간을 기준으로 오름차순 정렬
-        Arrays.sort(bookArr, (arr1, arr2) -> {
+        Arrays.sort(bookTime, (arr1, arr2) -> {
             if(arr1[0] == arr2[0]) return arr1[1] - arr2[1];
             return arr1[0] - arr2[0];
         });
         
-        // 대실 종료 시각을 저장하여 오름차순 정렬
-        PriorityQueue<Integer> pque = new PriorityQueue<>();
-        for(int i = 0; i < bookArr.length; i++) {
-            if(pque.isEmpty()) {
-                pque.offer(bookArr[i][1]);
-                answer++;
-                continue;
-            }
-            
-            // 현재 사용중인 방중에 종료된 방이 있으면 그방에 들어감
-            if(pque.peek() <= bookArr[i][0]) pque.poll();
-            else answer++;
-            
-            pque.offer(bookArr[i][1]);
+        for(int[] arr : bookTime) {
+            if(!pque.isEmpty() && pque.peek() + 10 <= arr[0]) pque.poll();
+            pque.offer(arr[1]);
         }
-        return answer;
+        
+        return pque.size();
     }
 }
